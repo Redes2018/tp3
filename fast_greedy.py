a9fb7c32bc0f71cba5c68174e0c1c26da1171375
Lucas Longo <lucaslongo52@gmail.com>
2018-10-23 01:01:49 -0300

    fast_greedy modularidad y silhouette 1.b

--- fast_greedy.py
+++ fast_greedy.py
@@ -69,6 +69,130 @@ plt.axis('off')
 
 plt.show()
 
+#-------------------------------------------------------------------------------
+#2.a) Modularidad y Silouhette:
+#-------------------------------------------------------------------------------
+#Asignamos a cada nodo del grafo la propiedad comunity que tendrá un color.
+
+for c,comu in enumerate(comunidades):
+    for delfin in list(comu):
+        mydolphins.node[delfin]['comunity']=colores[c]
+#-------------------------------------------------------------
+#Modularidad:
+#-------------------------------------------------------------
+#Matriz de Adyacencias Aij:
+delfines=mydolphins.nodes()
+A = np.array(nx.to_numpy_matrix(mydolphins,nodelist=delfines))
+N=mydolphins.number_of_nodes()
+m=mydolphins.number_of_edges()
+grado = dict(mydolphins.degree())
+
+#Matriz de variable categorica comunidad: Cij=delta(ci,cj)
+C=np.zeros(np.shape(A))
+for ni, idelfin in enumerate(delfines):
+    for nj, jdelfin in enumerate(delfines):
+        if mydolphins.nodes[idelfin]['comunity']==mydolphins.nodes[jdelfin]['comunity']:
+            C[ni,nj]=1
+
+#Calculo de la modularidad
+#Q/Qmax=(S1-S2)*(1/2m)
+
+#S1=Suma en nodos (Aij*Cij)
+#S2=Suma en nodos(kikj*Cij/2m)
+S1=0
+S2=0
+for ni, idelfin in enumerate(delfines):
+    for nj, jdelfin in enumerate(delfines):  
+        S1=S1+A[ni,nj]*C[ni,nj]
+        S2=S2+grado[idelfin]*grado[jdelfin]*C[ni,nj]
+S2=S2/(2*m)
+mod=(S1-S2)/(2*m)
+
+
+print('Modularidad Q = {0:.3f}'.format(mod))
+
+#-------------------------------------------------------------
+#Silhouette:
+#-------------------------------------------------------------
+#distancia promedio de un nodo a todos los de su comunidad
+#distancia se podria medir como el numero de links en el camino mas corto para llegar a otro.
+#Ej:
+#distancia entre 'Jet' y 'Trigger' = len(nx.shortest_path(mydolphins,'Jet','Trigger')) - 1
+
+#Calculo de a[i]
+a=[] #contendra para cada nodo en delfines la distancia promedio a nodos de su misma comunidad
+for idelfin in delfines:
+    distancias=[]
+    for jdelfin in delfines:
+        if mydolphins.nodes[idelfin]['comunity']==mydolphins.nodes[jdelfin]['comunity']:
+               distancias.append(len(nx.shortest_path(mydolphins,idelfin,jdelfin))-1)         
+    promedio=np.mean(distancias)
+    a.append(promedio)
+
+#Calculo de b[i]
+b=[] #contendria para cada nodo en delfines la distancia promedio a nodos de otras comunidades, devuelvo  el promedio minimo
+for idelfin in delfines: 
+    #Me quedo con las comunidades distintas a la del delfin idelfin
+    comuni=['blue','red','orange','green']
+    comuni.remove(mydolphins.node[idelfin]['comunity'])
+    b_comuni=[]
+    for c in comuni:
+        distancias=[]
+        for jdelfin in delfines:
+            if mydolphins.nodes[jdelfin]['comunity']==c:
+                distancias.append(len(nx.shortest_path(mydolphins,idelfin,jdelfin))-1)
+        promedio_comuni=np.mean(distancias)
+        b_comuni.append(promedio_comuni)
+    b.append(min(b_comuni))
+    comuni=['blue','red','orange','green'] #vuelvo a comuni al inicial
+
+#Calculo el Silhouette:
+print('Silhouette delfin-S[i]=')
+S=[]
+for i,idelfin in enumerate(delfines):
+    s=(b[i]-a[i])/(max(b[i],a[i]))
+    S.append(s)
+    #guardo la propiedad silhouette en el grafo:
+    mydolphins.node[idelfin]['silhouette']=s
+print (mydolphins.nodes.data('silhouette'))
+
+#Grafico de Silhouette:
+
+plt.figure(3)
+delfines=np.sort(delfines)
+x = []
+xTicks = []
+contador=-1
+dtype = [('name', 'S21'), ('silhouette', float)]
+for c in colores:
+    delf_S=[] #lista con pares de [delfin,silouette]
+    for i, idelfin in enumerate(delfines):
+        if mydolphins.nodes[idelfin]['comunity']==c:
+            delf_S.append((str(idelfin),mydolphins.nodes[idelfin]['silhouette']))        
+    delf_S=np.array(delf_S,dtype=dtype)
+    delf_S=np.sort(delf_S,order='silhouette') #lo ordeno segun la propiedad silhouette
+    delf_S=list(delf_S)
+    for j,d in enumerate(delf_S):
+        contador=contador+1
+        delfin=str(delf_S[j][0],'utf-8')
+        silhouette_value=delf_S[j][1]
+        plt.bar(contador,silhouette_value,color=mydolphins.nodes[delfin]['comunity'])#grafico una barra
+        x.append(contador)
+        xTicks.append(delfin)
+plt.xticks(x, xTicks)
+plt.xticks(range(len(delfines)),xTicks, rotation=90)
+plt.title('Silhouette',fontsize=20)
+plt.show()
+    
+ 
+            
+        
+        
+    
+        
+        
+    
+    
 
 
 
